68166cc5a73889884557f6731be9fc3efd2100ad
Raniel Mendonça <ranieel.m@gmail.com>
2023-04-12 13:36:52 -0300

    ajustes aula 03

--- src/components/ButtonCounter/index.tsx
+++ src/components/ButtonCounter/index.tsx
@@ -7,8 +7,6 @@ export interface IButtonCounterProps {
 
 const ButtonCounter = ({ title, active = true }: IButtonCounterProps) => {
   const [count, setCount] = useState(0);
-  console.log("🚀 ~ file: index.tsx:8 ~ ButtonCounter ~ title:", title);
-  console.log("🚀 ~ file: index.tsx:9 ~ ButtonCounter ~ active:", active);
 
   const handleClick = () => {
     setCount(count + 1);
